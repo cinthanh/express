991cdda2a117bed79ecc0763156c73b07b9bddbb
Thanhf Dev <thanhfgit@gmail.com>
2018-09-30 13:18:35 +0700

    create server node simple

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+app.listen(3333,function() {
+    console.log('Server running ...');
+});
