5273564f82431a230ed322d6ac7b210bfea8dd58
Thanhf Dev <thanhfgit@gmail.com>
2018-09-30 13:48:59 +0700

    node render view from static path

--- app.js
+++ app.js
@@ -4,6 +4,35 @@ var path = require('path');
 
 var app = express();
 
+// everything route after then.
+
+// middleware Node
+/*var logger = function(req, res, next) {
+    console.log('Logging ...');
+    next();
+}
+
+// apply middleware
+app.use(logger); */
+
+// apply body-parser is Middleware plugin
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Set static path | # node render views 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',function(req, res) {
+    res.send('Welcome Home'); // response is resulf return from server Node
+});
+
+
+
+// app.get('/hi',function(req,res) {
+//     res.send('debugbarJS');
+// });
 app.listen(3333,function() {
-    console.log('Server running ...');
+    console.log('Server running ... on port 3333');
 });
